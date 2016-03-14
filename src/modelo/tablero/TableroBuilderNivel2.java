dc3d7fe31eec4c490cdf7e2f5e290aa9d86ad182
Xabi Casado <xcasado003@ehu.eus>
2016-03-14 11:56:53 +0100

    Clases Builder del Tablero (III)

---
+++ src/modelo/tablero/TableroBuilderNivel2.java
@@ -0,0 +1,23 @@
+package modelo.tablero;
+
+public class TableroBuilderNivel2 extends TableroBuilderSinPersonalizar {
+
+	@Override
+	protected void definirFilas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void definirColumnas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void definirMinas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/modelo/tablero/TableroBuilderSinPersonalizar.java
@@ -0,0 +1,5 @@
+package modelo.tablero;
+
+public abstract class TableroBuilderSinPersonalizar extends TableroBuilder {
+
+}
