2ed0d2b9e0267f5ebe34b4cf68c997b54eb9d6f1
Andima Freire <afreire003@ehu.eus>
2016-03-14 11:53:50 +0100

    tablero

--- src/modelo/tablero/Tablero.java
+++ src/modelo/tablero/Tablero.java
@@ -5,4 +5,20 @@ public class Tablero {
 	private int columnas;
 	private int minas;
 	//private Coleccion<Casilla> casillas;
+	
+	public void setFilas(int filas) {
+		this.filas = filas;
+	}
+	public void setColumnas(int columnas) {
+		this.columnas = columnas;
+	}
+	public void setMinas(int minas) {
+		this.minas = minas;
+	}
+	public void desplegarCasilla(int pFila ,int pColumna ) {
+		//TODO Falta implementar
+	}
+	public void accionCasilla(int pFila, int pColumna){
+		//TODO Falta implementar
+	}
 }
