e70f8d832926b63e65bd55b816421db456e64879
Xabi Casado <xcasado003@ehu.eus>
2016-03-17 12:51:26 +0100

    Modificaciones en clases de modelo

---
+++ src/packModelo/Buscaminas.java
@@ -0,0 +1,28 @@
+package packModelo;
+
+import packModelo.packTablero.Tablero;
+import packModelo.packTablero.TableroBuilder;
+
+public class Buscaminas {
+	private static Buscaminas elBuscaminas;
+	private TableroBuilder tableroBuilder;
+	private Tablero tablero;
+	
+	private Buscaminas() { }
+	
+	public static Buscaminas getElBuscaminas() {
+		if (elBuscaminas == null) elBuscaminas = new Buscaminas();
+		return elBuscaminas;
+	}
+	
+	public void setTableroBuilder(TableroBuilder tableroBuilder) { this.tableroBuilder = tableroBuilder; }
+	public Tablero getTablero() { return tablero; }
+	
+	public void jugar() {
+		
+	}
+	
+	private void construirTablero() {
+		
+	}
+}
--- src/packModelo/packTablero/Tablero.java
+++ src/packModelo/packTablero/Tablero.java
@@ -1,23 +1,21 @@
 package packModelo.packTablero;
 
+import packModelo.packCasilla.Casilla;
+
 public class Tablero {
 	private int filas;
 	private int columnas;
 	private int minas;
-	//private Coleccion<Casilla> casillas;
+	private Casilla [][] casillas;
 	
-	public void setFilas(int filas) {
-		this.filas = filas;
-	}
-	public void setColumnas(int columnas) {
-		this.columnas = columnas;
-	}
-	public void setMinas(int minas) {
-		this.minas = minas;
-	}
+	public void setFilas(int filas) { this.filas = filas; }
+	public void setColumnas(int columnas) { this.columnas = columnas; }
+	public void setMinas(int minas) { this.minas = minas; }
+	
 	public void desplegarCasilla(int pFila ,int pColumna ) {
 		//TODO Falta implementar
 	}
+	
 	public void accionCasilla(int pFila, int pColumna){
 		//TODO Falta implementar
 	}
