225afcab86e336e660622fb43582a975160f9947
Xabi Casado <xcasado003@ehu.eus>
2016-03-18 09:53:55 +0100

    Corrección diagramas

--- src/packModelo/packCasilla/Casilla.java
+++ src/packModelo/packCasilla/Casilla.java
@@ -9,13 +9,15 @@ public abstract class Casilla {
 	private Coordenada coordenada;
 	
 	public Casilla(int pFila, int pColumna){
-		// TODO Completar constructor Casilla
 		coordenada = new Coordenada(pFila, pColumna);
 		estado = new Cubierta(coordenada);
 	}
+	
+	public Coordenada getCoordenada() { return this.coordenada; }
+	public void setEstado(Estado pEstado) { this.estado=pEstado; }
+	
 	public abstract void desplegarCasilla();
-	public Coordenada getCoordenada(){return this.coordenada;}
-	public void setEstado(Estado pEstado){estado=pEstado;}
+	
 	public void accionCasilla(){
 		this.estado.descubrirCasilla();
 	}
