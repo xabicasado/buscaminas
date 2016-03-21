c0077b6e9ac675f61fcf7643ef3fb38f4008c2ef
Xabi Casado <xcasado003@ehu.eus>
2016-03-21 10:56:35 +0100

    Añadidas clases de pruebas JUnit

---
+++ test/packModelo/packTablero/TableroTest.java
@@ -0,0 +1,89 @@
+package packModelo.packTablero;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TableroTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSetFilas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFilas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetColumnas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColumnas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMinas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMinas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCasillas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCasillas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDevolverCasilla() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIncrementarNumCasilla() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPonerCasilla() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDesplegarCasilla() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAccionCasilla() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDesplegarAdyacentes() {
+		fail("Not yet implemented");
+	}
+
+}
